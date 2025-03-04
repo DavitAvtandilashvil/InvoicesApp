b86133918ebeb50b06ec58f0d6ffcb21784efdf1
DavitAvtandilashvil <davit.avtandilashvili4@gmail.com>
2025-03-04 12:08:48 +0400

    add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle.ts";
 import Home from "./pages/Home.tsx";
 import AppLayout from "./AppLayout.tsx";
@@ -6,6 +6,7 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme.ts";
 import useInvoice from "./context/useInvoice.ts";
 import Auth from "./pages/Auth.tsx";
+import ProtectedRoute from "./ui/ProtectedRoute.tsx";
 
 function App() {
   const { isDarkMode } = useInvoice();
@@ -15,9 +16,12 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/auth" />} />
           <Route path="/auth" element={<Auth />} />
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<Home />} />
+          <Route element={<ProtectedRoute />}>
+            <Route element={<AppLayout />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
